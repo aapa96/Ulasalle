801c09a29d20e16b4d633531958d1e1836723412
aapa96 <aapa96@gmail.com>
2018-11-28 14:06:11 -0500

    test

--- Modulo/Ev4/routes.js
+++ Modulo/Ev4/routes.js
@@ -8,20 +8,20 @@ api.post('/editoriales/create',controlador.EditorialesCreate);
 api.get('/editoriales/read',controlador.EditorialesRead);
 api.get('/editoriales/read/:id',controlador.EditorialesReadOne);
 api.put('/editoriales/update',controlador.EditorialesUpdate);
-api.delete('/editoriales/delete',controlador.EditorialesDelete);
+api.delete('/editoriales/delete/:id',controlador.EditorialesDelete);
 
 api.post('/branchs/create',controlador.BranchCreate);
 api.get('/branchs/read',controlador.BranchRead);
 api.get('/branchs/read/:id',controlador.BranchReadOne);
 api.put('/branchs/update',controlador.BranchUpdate);
-api.delete('/branchs/delete',controlador.BranchDelete);
+api.delete('/branchs/delete/:id',controlador.BranchDelete);
 
 
 api.post('/organizations/create',controlador.OrganizationCreate);
 api.get('/organizations/read',controlador.OrganizationRead);
 api.get('/organizations/read/:id',controlador.OrganizationReadOne);
 api.put('/organizations/update',controlador.OrganizationUpdate);
-api.delete('/organizations/delete',controlador.OrganizationDelete);
+api.delete('/organizations/delete/:id',controlador.OrganizationDelete);
 
 api.post('/cruds/create',controlador.CrudCreate);
 api.get('/cruds/read',controlador.CrudRead);
