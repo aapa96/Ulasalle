1e2829e166fcd81630e2554351f886f9f3e1c014
aapa96 <aapa96@gmail.com>
2018-11-28 13:50:34 -0500

    test

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql');
+var port = process.env.PORT || 3977;
+if(port == 3977){
 
-var con = mysql.createConnection({
-  host: "172.245.173.170",
-  port: '3306',
-  user: "ulibrary",
-  password: "12345678",
-  database: 'library'
-});
+  var con = mysql.createConnection({
+    host: "172.245.173.170",
+    port: '3306',
+    user: "ulibrary",
+    password: "12345678",
+    database: 'library'
+  });
+}
+
 
 con.connect(function(err) {
   if (err) {
